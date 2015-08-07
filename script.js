e0bff563757d13b822615c4228b651e65aa684bf
Hiram <blujay270@gmail.com>
2015-08-07 15:14:39 -0400

    added style.css & highlighting key functionality in script.js

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ var context = new AudioContext();
 var oscillator;
 var mouseDown = 0;
 var playing = 0;
+var lastKey;
 
 function stop() {
   if (playing == 1) {
   	oscillator.stop(context.currentTime);
+	switch(lastKey.className.animVal) {
+	  case "white": lastKey.style.fill = "white"; break;
+	  case "black": lastKey.style.fill = "black"; break;
+	  default: lastKey.style.fill = "blue";
+	}
   	--playing;
+	return;
   }
+  switch(lastKey.className.animVal) {
+    case "white": lastKey.style.fill = "white"; break;
+    case "black": lastKey.style.fill = "black"; break;
+    default: lastKey.style.fill = "blue";
+  }
+
 }
 
 document.onmousedown = function() {
@@ -19,7 +32,9 @@ document.onmouseup = function() {
   --mouseDown;
 }
 
-function play(freq, clicked) {
+function play(freq, clicked, id) {
+  var key = document.getElementById(id);
+  lastKey = key;
   if (mouseDown == 1) {
     stop();
   }
@@ -29,6 +44,17 @@ function play(freq, clicked) {
     oscillator.type = 'sawtooth';
     oscillator.frequency.value = Number(freq);
     oscillator.start(context.currentTime);
+    switch(key.className.animVal) {
+      case "white": key.style.fill = "#BBDDBB"; break;
+      case "black": key.style.fill = "#224422"; break;
+      default: key.style.fill = "red";
+    }
     ++playing;
+    return;
+  }
+  switch(key.className.animVal) {
+    case "white": key.style.fill = "#DDFFDD"; break;
+    case "black": key.style.fill = "#446644"; break;
+    default: key.style.fill = "red";
   }
 }
