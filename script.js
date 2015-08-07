92190902f2d569c71546b248a798d825f50a6b23
Hiram <blujay270@gmail.com>
2015-08-07 02:16:31 -0400

    made piano slidable on keys & fixed general bugs

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ var context = new AudioContext();
 
 var oscillator;
 
-function play(freq) {
-  	oscillator = context.createOscillator();
-	oscillator.connect(context.destination);
-	oscillator.type = 'sawtooth';
-	oscillator.frequency.value = Number(freq);
-	oscillator.start(context.currentTime);
+var mouseDown = 0;
+
+document.onmousedown = function() {
+  ++mouseDown;
 }
 
-function stop() {
-	oscillator.stop(context.currentTime);
+document.onmouseup = function() {
+  oscillator.stop(context.currentTime);
+  --mouseDown;
+}
+
+function play(freq, clicked) {
+  if (mouseDown == 1) {
+    oscillator.stop(context.currentTime);
+  }
+  if (mouseDown == 1 || clicked == true) {
+    oscillator = context.createOscillator();
+    oscillator.connect(context.destination);
+    oscillator.type = 'sawtooth';
+    oscillator.frequency.value = Number(freq);
+    oscillator.start(context.currentTime);
+  }
 }
